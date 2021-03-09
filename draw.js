cabdc8d74c409450fa7577a6d716505f4a156971
Anastasia Miheeva <mikheevaanastasiia93@yandex.ru>
2021-03-09 18:37:53 +0300

    imgs added

--- draw.js
+++ draw.js
@@ -1,176 +1,166 @@
-window.addEventListener('load', function() {
-  //константы 
- let GAME_WIDTH = 640;
- let GAME_HEIGHT = 360;
+window.addEventListener('load', function(){
+  //константы
+  let GAME_WIDTH = 640;
+  let GAME_HEIGHT = 360;
 
- let gameLive = true;
- let gameOver = false;
- let finishGame = false;
- 
- //враги
- let enemies = [
-   {
-     x: 100, 
-     y: 100, 
-     speedY: 1, 
-     w: 40, 
-     h: 40, 
-     color: "#BA55D3"
-   },
-   {
-     x: 260,
-     y: 100,
-     speedY: 2,
-     w: 40,
-     h: 40,
-     color: "#0048BA"
-   },
-   {
-     x: 380,
-     y: 100,
-     speedY: 3,
-     w: 40,
-     h: 40,
-     color: "#0FC0FC"
-   },
-   {
-     x: 550,
-     y: 100,
-     speedY: 1,
-     w: 40,
-     h: 40,
-     color: "#40E0D0"
-   }
- ];
+  let gameLive = true;
 
- //объект игрока
-  let player = { 
-    x1: 10, 
-    y1: 160, 
-    x2: 40, 
-    y2: 180,
-    x3: 10, 
-    y3: 200, 
-    speedX: 2,
-    isMoving: false,
-    color: "#00FF00"
+  //враги
+  let enemies = [
+    {
+      x: 100, 
+      y: 100, 
+      speedY: 1, 
+      w: 40, 
+      h: 40 
+    },
+    {
+      x: 260,
+      y: 100,
+      speedY: 2,
+      w: 40,
+      h: 40
+    },
+    {
+      x: 380,
+      y: 100,
+      speedY: 1,
+      w: 40,
+      h: 40
+    },
+    {
+      x: 450,
+      y: 100,
+      speedY: 2,
+      w: 40,
+      h: 40
+    }
+  ];
+//обьет игрока
+  let player = {
+    x:10,
+    y:160,
+    speedX:2,
+    w:40,
+    h:40,
+    isMoving:false
+  };
+
+  //finish
+  let goal = {
+    x:580,
+    y:160,
+    w:50,
+    h:36
   }
+  let sprites = {};
 
-  let movePlayer = () => {
+  //movePlayer
+  let movePlayer = function(){
     player.isMoving = true;
   };
 
-  let stopPlayer = () => {
+  //stopPlayer
+  let stopPlayer = function(){
     player.isMoving = false;
-  }
+  };
+    //получить контекст
+    let canvas = document.getElementById("mycanvas");
+    let ctx = canvas.getContext("2d");
 
-  //получаем холст и контекст
-  let canvas = document.getElementById("mycanvas");
-  let ctx = canvas.getContext("2d");
 
-  //addEventListener на объект игрока
-  canvas.addEventListener("mousedown", movePlayer);
-  canvas.addEventListener("mouseup", stopPlayer);
-  canvas.addEventListener("touchstart", movePlayer);
-  canvas.addEventListener("touchend", stopPlayer);
+    //addEventListener на обьект игрока
+    canvas.addEventListener("mousedown", movePlayer);
+    canvas.addEventListener("mouseup", stopPlayer);
+    canvas.addEventListener('touchstart', movePlayer);
+    canvas.addEventListener('touchend', stopPlayer);
 
-  //логика объектов
-  let update = () => {
-    //игрок
-    if(player.isMoving) {
-      player.x1 += player.speedX
-      player.x2 += player.speedX
-      player.x3 += player.speedX
+    let load = function() {
+      sprites.player = new Image();
+      sprites.player.src = 'img/hero.png';
+
+      sprites.background = new Image();
+      sprites.background.src = 'img/floor.png';
+
+      sprites.enemy = new Image();
+      sprites.enemy.src = 'img/enemy.png';
+
+      sprites.goal = new Image();
+      sprites.goal.src = 'img/chest.png';
+    };
+
+
+    //логика обьектов
+  let update = function() {
+
+    //проверка на финиш
+    if(chekCollision(player, goal)){
+      //stop game
+      gameLive = false;
+
+      alert ('You WIN');
     }
-
-    if(player.x2 > GAME_WIDTH) {
-      finishGame = true;
+      //игрок
+    if(player.isMoving) {
+      player.x = player.x + player.speedX;
     }
 
     //враги
-    let i = 0;
-    let n = enemies.length;
+    enemies.forEach(function(element, index){
 
-    enemies.forEach(element => { 
-      if(chekCollision(player, element)) {
-        //остановка игры
-        gameLive = false;
-        gameOver = true;
-      }
-      element.y += element.speedY;
-      //соприкосновение объектов
-      if(element.y <= 10) {
-        element.y = 10;
-        element.speedY = element.speedY * -1; //element.speedY *= -1
-      } 
-      else if (element.y >= GAME_HEIGHT - 50) {
+    if(chekCollision(player, element)){
+      //stop
+      gameLive = false;
+
+      alert("Game OVER");
+    window.location = "";
+    }
+    element.y += element.speedY;
+
+  //соприкосновения обьектов
+    if(element.y <= 10) {
+      element.y = 10;
+      element.speedY *= -1;
+    } else if(element.y >= GAME_HEIGHT - 50) {
         element.y = GAME_HEIGHT - 50;
-        element.speedY = element.speedY * -1;
+        element.speedY *= -1;
       }
-    })
-  };
-  
-  //показать объекты на экране
-  let draw = () => {
-    //чистим холст
-    ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
-
-    //объект игрока
-    ctx.fillStyle = player.color;
-    ctx.beginPath();
-    ctx.moveTo(player.x1,player.y1);
-    ctx.lineTo(player.x2,player.y2);
-    ctx.lineTo(player.x3,player.y3);
-    ctx.fill();
-
-    //объекты врагов
-    enemies.forEach(element => {
-      ctx.fillStyle = element.color
-      ctx.fillRect(element.x, element.y, element.w, element.h)
     });
   };
 
-  let text = (color, fnt, txt, x, y) => {
-    ctx.fillStyle = color;
-    ctx.font = fnt;
-    ctx.fillText(txt, x, y)
-  }
+  //показать обьекты на экране.
+  let draw = function(){
+    //чистим холст
+    ctx.clearRect(0,0,GAME_WIDTH,GAME_HEIGHT);
 
-  let rect = (x, y, w, h, c) => {
-    ctx.fillStyle = c;
-    ctx.fillRect(x, y, w, h);
-  }
+  //background
+    ctx.drawImage(sprites.background, 0, 0);
 
-  //инициализируем несколько раз в секунду
+  //обькт игрока
+    ctx.drawImage(sprites.player, player.x, player.y);
+
+  //обьекты врагов
+    enemies.forEach(function(element){
+      ctx.drawImage(sprites.enemy, element.x, element.y);
+    });
+
+  //обьект финиш
+    ctx.drawImage (sprites.goal, goal.x, goal.y);
+  };
+  //инициализируем несколько раз в секунду)
   let step = function() {
+
     update();
     draw();
-
     if(gameLive){
       window.requestAnimationFrame(step);
     }
-
-    if(gameOver) {
-      rect(0, 0, canvas.width, canvas.height, 'black');
-      text('white', '75px Exo', 'GAME OVER', 100, 200)
-      setTimeout(restart, 2000)
-    }
-
-    if(finishGame) {
-      rect(0, 0, canvas.width, canvas.height, 'green');
-      text('white', '75px Exo', 'FINISH', 200, 200)
-    }
   };
-
-  let restart = () => {
-    window.location = ""
-  }
-
-  let chekCollision = (point, rect) => {
-    let closeOnWidth = Math.abs(point.x1 - rect.x) <= Math.abs((point.x2 - point.x1), rect.w);
-    let closeOnHeight = Math.abs((point.y1-rect.y)) <= Math.abs((point.y3-point.y1), rect.h);
+  let chekCollision = function(rect1, rect2){
+    let closeOnWidth= Math.abs(rect1.x - rect2.x) <= Math.abs(rect1.w, rect2.w);
+    let closeOnHeight = Math.abs(rect1.y - rect2.y) <= Math.abs(rect1.h, rect2.h);
     return closeOnHeight && closeOnWidth;
-  } 
-  
+  }
+  load();
   step();
-})
+});
